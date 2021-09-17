63853298f81a1e9ecd1878f081131687182d8242
techyoshino <83795891+techyoshino@users.noreply.github.com>
2021-09-17 16:07:48 +0900

    yohino-2021-09-17

--- wp-content/themes/edge-child/front-page.php
+++ wp-content/themes/edge-child/front-page.php
@@ -85,11 +85,22 @@ $edge_settings = edge_get_theme_options(); ?>
       </div>
       <nav  id="slide_menu" class="accordion_nav">
         <ul>
-      <li><a href="#section1">Section1</a></li>
+      		
+		  <!-- <li><a href="#section1">Section1</a></li>
           <li><a href="#section2">Section2</a></li>
           <li><a href="#section3">Section3</a></li>
           <li><a href="#section4">Section4</a></li>
+		  <li><a href="#section5">Section5</a></li> -->
+
         </ul>
+
+		<!-- テンプレートファイルのメニューを表示したい場所に記述 -->
+		<?php 
+		wp_nav_menu( array( 
+			'theme_location' => 'test-menu' 
+		) ); 
+		?>
+
       </nav>
     </div>
   </div>  
@@ -107,91 +118,135 @@ $edge_settings = edge_get_theme_options(); ?>
 
     <section class="section sec3">
       <h2>Section3</h2>
+	</section>
+
+	<section class="section sec4">
+      <h2>Section4</h2>
     </section>
 
-    <section class="section sec4">
-      <footer id="colophon" class="site-footer clearfix">
-<?php
-if ( is_front_page() && is_home() ) {
-	if ((function_exists('display_instagram')) && $edge_settings['edge_instagram_feed_display'] !=0){
-		echo do_shortcode('[instagram-feed]');
-	}// Default homepage
-} elseif ( is_front_page()){
-	if ((function_exists('display_instagram')) && $edge_settings['edge_instagram_feed_display'] !=0){
-		echo do_shortcode('[instagram-feed]');
-	}//Static homepage
-} else {
-//silence is golden
-}
-$footer_column = $edge_settings['edge_footer_column_section'];
-	if( is_active_sidebar( 'edge_footer_1' ) || is_active_sidebar( 'edge_footer_2' ) || is_active_sidebar( 'edge_footer_3' ) || is_active_sidebar( 'edge_footer_4' )) { ?>
-	<div class="widget-wrap">
-		<div class="container">
-			<div class="widget-area clearfix">
-			<?php
-				if($footer_column == '1' || $footer_column == '2' ||  $footer_column == '3' || $footer_column == '4'){
-				echo '<div class="column-'.$footer_column.'">';
-					if ( is_active_sidebar( 'edge_footer_1' ) ) :
-						dynamic_sidebar( 'edge_footer_1' );
-					endif;
-				echo '</div><!-- end .column'.$footer_column. '  -->';
-				}
-				if($footer_column == '2' ||  $footer_column == '3' || $footer_column == '4'){
-				echo '<div class="column-'.$footer_column.'">';
-					if ( is_active_sidebar( 'edge_footer_2' ) ) :
-						dynamic_sidebar( 'edge_footer_2' );
-					endif;
-				echo '</div><!--end .column'.$footer_column.'  -->';
-				}
-				if($footer_column == '3' || $footer_column == '4'){
-				echo '<div class="column-'.$footer_column.'">';
-					if ( is_active_sidebar( 'edge_footer_3' ) ) :
-						dynamic_sidebar( 'edge_footer_3' );
-					endif;
-				echo '</div><!--end .column'.$footer_column.'  -->';
-				}
-				if($footer_column == '4'){
-				echo '<div class="column-'.$footer_column.'">';
-					if ( is_active_sidebar( 'edge_footer_4' ) ) :
-						dynamic_sidebar( 'edge_footer_4' );
-					endif;
-				echo '</div><!--end .column'.$footer_column.  '-->';
-				}
-				?>
-			</div> <!-- end .widget-area -->
-		</div> <!-- end .container -->
-	</div> <!-- end .widget-wrap -->
-	<?php } ?>
-<div class="site-info" <?php if($edge_settings['edge-img-upload-footer-image'] !=''){?>style="background-image:url('<?php echo esc_url($edge_settings['edge-img-upload-footer-image']); ?>');" <?php } ?>>
-	<div class="container">
-	<?php
-		if($edge_settings['edge_buttom_social_icons'] == 0):
-			do_action('social_links');
-		endif;
-		do_action('edge_footer_menu');
+	
 
-		if ( is_active_sidebar( 'edge_footer_options' ) ) :
-		dynamic_sidebar( 'edge_footer_options' );
-		else:
-			echo '<div class="copyright">' .'&copy; ' . date('Y') .' '; ?>
-		<a title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" target="_blank" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo get_bloginfo( 'name', 'display' ); ?></a> 
-				
-		<?php /* ?>デフォルトのコピーライト非表示						
-		<?php esc_html_e('Designed by:','edge'); ?> <a title="<?php echo esc_attr__( 'Theme Freesia', 'edge' ); ?>" target="_blank" href="<?php echo esc_url( 'https://themefreesia.com' ); ?>"><?php esc_html_e('Theme Freesia','edge');?></a> | 
-							<?php esc_html_e('Powered by:','edge'); ?> <a title="<?php echo esc_attr__( 'WordPress', 'edge' );?>" target="_blank" href="<?php echo esc_url( 'http://wordpress.org' );?>"><?php esc_html_e('WordPress','edge'); ?></a>
-							<?php */ ?>
-		
+    <section class="section sec5">
+
+		<div class="new_selected_Ctr">
+
+			<?php
+			$information= get_posts( array(
+				'posts_per_page' => 5, // 表示件数の指定
+				'category' => 13 // カテゴリーID 1 のカテゴリーを指定
+			));
+			if( $information):
+			?>
+				<ul class="new_selected">
+					<?php
+					foreach( $information as $post ):
+					setup_postdata( $post );
+					?>
+					<li><span class="new_date"><?php the_time('Y年n月j日'); ?></span><a href="<?php the_permalink(); ?>"> <strong class="new_title"><?php the_title(); ?></strong> <span class="new_txt"><?php echo mb_substr( get_the_excerpt(), 0, 10 ) . '...'; ?></span></a></li>
+					<?php
+					endforeach;
+					wp_reset_postdata();
+					?>
+				</ul>
+			<?php else: ?>
+			<p>表示できる情報はありません。</p>
+			<?php endif; ?>
+
+			<div class="new_selected_btn">
+
+				<a href="<?php echo home_url(); ?>/info/" class="arrow sample">viewmore</a>
+
 			</div>
-		<?php endif; ?>
-			<div style="clear:both;"></div>
-		</div> <!-- end .container -->
-	</div> <!-- end .site-info -->
-	<?php
-		$disable_scroll = $edge_settings['edge_scroll'];
-		if($disable_scroll == 0):?>
-	<div class="go-to-top"><a title="<?php esc_html_e('Go to Top','edge');?>" href="#masthead"><i class="fa fa-angle-double-up"></i></a></div> <!-- end .go-to-top -->
-	<?php endif; ?>
-</footer> <!-- end #colophon -->
+
+
+		</div>
+
+
+
+					
+
+      <footer id="colophon" class="site-footer clearfix">
+		<?php
+		if ( is_front_page() && is_home() ) {
+			if ((function_exists('display_instagram')) && $edge_settings['edge_instagram_feed_display'] !=0){
+				echo do_shortcode('[instagram-feed]');
+			}// Default homepage
+		} elseif ( is_front_page()){
+			if ((function_exists('display_instagram')) && $edge_settings['edge_instagram_feed_display'] !=0){
+				echo do_shortcode('[instagram-feed]');
+			}//Static homepage
+		} else {
+		//silence is golden
+		}
+		$footer_column = $edge_settings['edge_footer_column_section'];
+			if( is_active_sidebar( 'edge_footer_1' ) || is_active_sidebar( 'edge_footer_2' ) || is_active_sidebar( 'edge_footer_3' ) || is_active_sidebar( 'edge_footer_4' )) { ?>
+			<div class="widget-wrap">
+				<div class="container">
+					<div class="widget-area clearfix">
+					<?php
+						if($footer_column == '1' || $footer_column == '2' ||  $footer_column == '3' || $footer_column == '4'){
+						echo '<div class="column-'.$footer_column.'">';
+							if ( is_active_sidebar( 'edge_footer_1' ) ) :
+								dynamic_sidebar( 'edge_footer_1' );
+							endif;
+						echo '</div><!-- end .column'.$footer_column. '  -->';
+						}
+						if($footer_column == '2' ||  $footer_column == '3' || $footer_column == '4'){
+						echo '<div class="column-'.$footer_column.'">';
+							if ( is_active_sidebar( 'edge_footer_2' ) ) :
+								dynamic_sidebar( 'edge_footer_2' );
+							endif;
+						echo '</div><!--end .column'.$footer_column.'  -->';
+						}
+						if($footer_column == '3' || $footer_column == '4'){
+						echo '<div class="column-'.$footer_column.'">';
+							if ( is_active_sidebar( 'edge_footer_3' ) ) :
+								dynamic_sidebar( 'edge_footer_3' );
+							endif;
+						echo '</div><!--end .column'.$footer_column.'  -->';
+						}
+						if($footer_column == '4'){
+						echo '<div class="column-'.$footer_column.'">';
+							if ( is_active_sidebar( 'edge_footer_4' ) ) :
+								dynamic_sidebar( 'edge_footer_4' );
+							endif;
+						echo '</div><!--end .column'.$footer_column.  '-->';
+						}
+						?>
+					</div> <!-- end .widget-area -->
+				</div> <!-- end .container -->
+			</div> <!-- end .widget-wrap -->
+			<?php } ?>
+		<div class="site-info" <?php if($edge_settings['edge-img-upload-footer-image'] !=''){?>style="background-image:url('<?php echo esc_url($edge_settings['edge-img-upload-footer-image']); ?>');" <?php } ?>>
+			<div class="container">
+			<?php
+				if($edge_settings['edge_buttom_social_icons'] == 0):
+					do_action('social_links');
+				endif;
+				do_action('edge_footer_menu');
+
+				if ( is_active_sidebar( 'edge_footer_options' ) ) :
+				dynamic_sidebar( 'edge_footer_options' );
+				else:
+					echo '<div class="copyright">' .'&copy; ' . date('Y') .' '; ?>
+				<a title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" target="_blank" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo get_bloginfo( 'name', 'display' ); ?></a> 
+						
+				<?php /* ?>デフォルトのコピーライト非表示						
+				<?php esc_html_e('Designed by:','edge'); ?> <a title="<?php echo esc_attr__( 'Theme Freesia', 'edge' ); ?>" target="_blank" href="<?php echo esc_url( 'https://themefreesia.com' ); ?>"><?php esc_html_e('Theme Freesia','edge');?></a> | 
+									<?php esc_html_e('Powered by:','edge'); ?> <a title="<?php echo esc_attr__( 'WordPress', 'edge' );?>" target="_blank" href="<?php echo esc_url( 'http://wordpress.org' );?>"><?php esc_html_e('WordPress','edge'); ?></a>
+									<?php */ ?>
+				
+					</div>
+				<?php endif; ?>
+					<div style="clear:both;"></div>
+				</div> <!-- end .container -->
+			</div> <!-- end .site-info -->
+			<?php
+				$disable_scroll = $edge_settings['edge_scroll'];
+				if($disable_scroll == 0):?>
+			<div class="go-to-top"><a title="<?php esc_html_e('Go to Top','edge');?>" href="#masthead"><i class="fa fa-angle-double-up"></i></a></div> <!-- end .go-to-top -->
+			<?php endif; ?>
+		</footer> <!-- end #colophon -->
     </section>
 
   </div>
